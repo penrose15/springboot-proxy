50041dcbafd4fbb1aaaf9b9a889dc272b8d051f8
penrose15 <seongju99@naver.com>
2023-06-17 23:55:41 +0900

    proxyFactory, postbeanprocessor

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -6,6 +6,8 @@ import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.interface_proxy.InterfaceProxyConfig;
 import hello.proxy.config.v2_dynamicProxy.DynamicProxyBasicConfig;
 import hello.proxy.config.v2_dynamicProxy.DynamicProxyFilterConfig;
+import hello.proxy.config.v3_proxy.ProxyFactoryConfigV1;
+import hello.proxy.config.v3_proxy.ProxyFactoryConfigV2;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -18,7 +20,9 @@ import org.springframework.context.annotation.Import;
 //@Import(InterfaceProxyConfig.class)
 //@Import(ConcreteProxyConfig.class)
 //@Import(DynamicProxyBasicConfig.class)
-@Import(DynamicProxyFilterConfig.class)
+//@Import(DynamicProxyFilterConfig.class)
+//@Import(ProxyFactoryConfigV1.class)
+@Import(ProxyFactoryConfigV2.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의 하위 패키지만 컴포넌트 스캔 함
 public class ProxyApplication {
 
